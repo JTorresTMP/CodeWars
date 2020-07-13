6b7a0e76b8ed159fe91680347121986cc033276c
Jose_Torres <torresjose6541@gmail.com>
2020-07-13 14:28:19 -0700

    Got the Solution!

--- Kyu7/sum_of_a_sequence.py
+++ Kyu7/sum_of_a_sequence.py
@@ -11,20 +11,18 @@
 # sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
 # sequenceSum(1,5,3) === 5 // 1 + 4
 
-from math import ceil
+import math
 def sequence_sum(begin, end, step):
     if begin > end:
         return 0
     else:
-        terms = ceil((((end - begin) / step) + 1))
-        # ans = (length * (begin + end)) / 2
+        terms = math.floor((((end - begin) / step) + 1))
         summation = (terms / 2) * ((2 * begin) + ((terms -1) * step))
-        # print(terms)
         return summation
-        # return sum([x for x in range(begin, end + 1, step)])
 
-print(sequence_sum(2,2,2)) #2
-print(sequence_sum(2,6,2)) #6
-print(sequence_sum(1,5,1)) #15
-print(sequence_sum(0,15,3)) #45
-print(sequence_sum(1,5,3)) #5
+print(sequence_sum(2,2,2)) #2, 1 Term
+print(sequence_sum(2,6,2)) #6, 3 Terms
+print(sequence_sum(1,5,1)) #15, 5 Terms
+print(sequence_sum(0,15,3)) #45, 6 Terms
+print(sequence_sum(1,5,3)) #5, 2 Terms
+print(sequence_sum(1, 15, 3))
